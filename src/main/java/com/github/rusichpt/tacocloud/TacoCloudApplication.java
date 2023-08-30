d7a5e60bec5938a01a1b8bebb2402621ba0a13db
pavel.tokarev <tokarevpavel1996@mail.ru>
2023-08-30 17:22:32 +0300

    Скелет приложения и домашняя страница

---
+++ src/main/java/com/github/rusichpt/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,19 @@
+package com.github.rusichpt.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+    @Bean
+    String string(){
+        return new String("1");
+    }
+
+}
